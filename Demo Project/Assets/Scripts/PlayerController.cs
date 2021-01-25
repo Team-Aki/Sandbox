592d0c06c65c7b0a489df4645153532b62b5ea3a
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-01-25 15:06:24 +0000

    Movement
    Done

--- Demo Project/Assets/Scripts/PlayerController.cs
+++ Demo Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
+    public CharacterController controller;
     public Transform cam;
 
     //Movement Settings
